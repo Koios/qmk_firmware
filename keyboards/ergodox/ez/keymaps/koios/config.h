72e57bf218a5b5cfdd34ea1c9c22af9a3953ab30
koios <koios@programmingpanda.net>
2017-07-11 08:11:04 +0200

    Also include parent config.h

--- keyboards/ergodox/ez/keymaps/koios/config.h
+++ keyboards/ergodox/ez/keymaps/koios/config.h
@@ -1,4 +1,13 @@
+#ifndef ERGODOX_EZ_KOIOS_CONFIG_H
+#define ERGODOX_EZ_KOIOS_CONFIG_H
+
+
+#include "../../config.h"
+
 #define PERMISSIVE_HOLD 1
 
 #define COMBO_ENABLE 1
 #define COMBO_COUNT 1
+
+
+#endif
