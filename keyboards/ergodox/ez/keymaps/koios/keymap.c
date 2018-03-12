f63f16228414c32cc1bec2800cbd39a06de9d08b
koios <koios@programmingpanda.net>
2018-03-12 08:36:19 +0100

    Make ~/ easier to type

--- keyboards/ergodox/ez/keymaps/koios/keymap.c
+++ keyboards/ergodox/ez/keymaps/koios/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,         KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                         KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
 // TODO: lonely ampersand
-       KC_TRNS,         KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+       KC_TRNS,         KC_AMPR, KC_1,   KC_2,    KC_3,    KC_TILD, KC_TRNS,
                                  KC_0,   KC_DOT,  KC_NO,   KC_EQL,  KC_TRNS,
 
        KC_TRNS, KC_TRNS,
